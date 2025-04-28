af377a897e58a32b884211bd239c1a75f047f144
s_baireddy\shanmukibaireddy <shanmukhi@mgtinsurance.com>
2025-04-28 15:37:10 -0600

    database

--- app/query/route.ts
+++ app/query/route.ts
@@ -11,31 +11,31 @@
    `;
 
  	return data;
- }
+ } 
 
- async function sample() {
+ /*async function sample() {
   const data = await sql`
     SELECT 1
   `;
 
   return data;
-}
+} */
 
 export async function GET() {
   //return Response.json({
    // message:
       //'Uncomment this file and remove this line. You can delete this file when you are finished.',
   //});
-   /*try {
-   	return Response.json(await sample());
+   try {
+   	return Response.json(await listInvoices());
    } catch (error) {
    	return Response.json({ error }, { status: 500 });
-   } */
-     try {
+   } 
+    /* try {
       const response = await fetch('https://ep-black-queen-a569ho0p-pooler.us-east-2.aws.neon.tech');
       return Response.json(response);
-      console.log('Fetch response:', response.status);
+      //console.log('Fetch response:', response.status);
     } catch (err) {
       console.error('Fetch test error:', err);
-    }
+    } */
 }
